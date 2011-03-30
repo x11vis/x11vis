f6e7a10bd08763f32472a87b66fd29e6a793f27a
Michael Stapelberg <michael@stapelberg.de>
2011-03-30 11:26:24 +0200

    Implement remote endpoint lookup, use TCP, commit webinterface server

--- interceptor/interceptor.pl
+++ interceptor/interceptor.pl
@@ -6,15 +6,75 @@
 
 use strict;
 use warnings;
+use Socket qw(sockaddr_in);
 use AnyEvent::Socket;
 use AnyEvent;
+use Twiggy::Server;
 use lib qw(lib);
 use ProxyConn;
+use Web;
+use IO::Handle;
+use IO::All;
 use v5.10;
 
-# provide /tmp/.X11-unix/X8
-my $server = tcp_server "unix/", "/tmp/.X11-unix/X8", sub {
+#
+# Figure out the cmdline of the remote endpoint of the given TCP socket file
+# descriptor $fileno by examining /proc/ (only works if the endpoint is on
+# the local system, of course)
+#
+sub endpoint_cmdline {
+    my ($fh) = @_;
+
+    my ($port, $addr) = sockaddr_in(getpeername($fh));
+    my @bytes = unpack('C4', $addr);
+    my $remote = sprintf("%02X%02X%02X%02X:%04X",
+        $bytes[3], $bytes[2], $bytes[1], $bytes[0], $port);
+
+    # get the info line from /proc/net/tcp for the remote endpoint
+    my ($info) = grep { /^\s+[^:]+: \b$remote\b/ } io('/proc/net/tcp')->slurp;
+
+    # extract the inode of the remote endpoint
+    $info =~ s/^\s+//g;
+    $info =~ s/\s+/ /g;
+    my $remote_inode = (split(/ /, $info))[9];
+
+    # find the corresponding process which has a link to this inode
+    for (</proc/*/fd/*>) {
+        my $target = readlink or next;
+        next unless $target =~ /^socket:\[$remote_inode\]$/;
+        my ($pid) = ($_ =~ m,/proc/([0-9]+)/,);
+        # return its commandline
+        my $cmdline = io("/proc/$pid/cmdline")->slurp;
+        $cmdline =~ s/\0/ /g;
+        return $cmdline;
+    }
+
+    return '<fd ' . $fh->fileno . '>';
+}
+
+
+## provide /tmp/.X11-unix/X8
+#my $server = tcp_server "unix/", "/tmp/.X11-unix/X7", sub {
+#    my ($fh, $host, $port) = @_;
+#
+#    say "fh = $fh, fileno = " . $fh->fileno;
+#
+#    ProxyConn->new(
+#        fh => $fh,
+#        host => $host,
+#        port => $port
+#    );
+#}, sub {
+#    my ($fh, $thishost, $thisport) = @_;
+#    warn "bound to $thishost, port $thisport\n";
+#};
+
+# We need to use TCP so that we can do a remote endpoint lookup
+my $tcp_server = tcp_server "127.0.0.1", "6000", sub {
     my ($fh, $host, $port) = @_;
+
+    my $cmdline = endpoint_cmdline($fh);
+    say "cmdline = $cmdline";
 
     ProxyConn->new(
         fh => $fh,
@@ -23,7 +83,21 @@ my $server = tcp_server "unix/", "/tmp/.X11-unix/X8", sub {
     );
 }, sub {
     my ($fh, $thishost, $thisport) = @_;
-    warn "bound to $thishost, port $thisport\n";
+    warn "(tcp) bound to $thishost, port $thisport\n";
 };
+
+# setup the web interface
+my $twiggy = Twiggy::Server->new(
+    host => '0.0.0.0',
+    port => '5523'
+);
+
+my $app = sub {
+    my $env = shift;
+    my $req = Dancer::Request->new($env);
+    Dancer->dance($req);
+};
+
+$twiggy->register_service($app);
 
 AE->cv->recv
---
+++ interceptor/lib/Web.pm
@@ -0,0 +1,45 @@
+# vim:ts=4:sw=4:expandtab
+# x11vis - an X11 protocol visualizer
+# © 2011 Michael Stapelberg and contributors (see ../LICENSE)
+#
+# XXX: This is just a proof of concept implementation to providing the
+# XXX: webinterface. It’s quick and dirty and needs to be cleaned up.
+#
+package Web;
+
+use strict;
+use warnings;
+use Dancer ':syntax';
+use x11vis;
+use IO::All;
+use v5.10;
+
+get '/' => sub {
+    # for now, we redirect to /gui/
+    redirect '/gui/';
+};
+
+get '/gui/' => sub {
+    return io('../gui/poc.html')->slurp;
+};
+
+get '/gui/:file' => sub {
+    if (params->{file} =~ /\.css$/) {
+        header 'Content-Type' => 'text/css';
+    }
+    #sendfile('../gui/' . params->{file});
+    return io('../gui/' . params->{file})->slurp;
+};
+
+get '/tracedata/output.json' => sub {
+    #mime_type 'json' => 'application/json';
+    #send_file('/tmp/output.json');
+    header 'Content-Type' => 'application/json';
+    return io('output.json')->slurp . ']';
+};
+
+get '/templates/:file' => sub {
+    return io('../gui/templates/' . params->{file})->slurp;
+};
+
+1
